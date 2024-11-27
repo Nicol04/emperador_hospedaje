ab9cf8ad47884e5a5398771a7a718ba1c54f5282
MirkoDx04 <midelgadol@ucvvirtual.edu.pe>
2024-11-26 22:21:48 -0500

    Update ConfirmPasswordController.php

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -5,10 +5,29 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ConfirmsPasswords;
 
+ * Controlador para gestionar la confirmación de contraseñas.
+ * 
+ * Este controlador utiliza el trait `ConfirmsPasswords` para manejar la lógica
+ * de confirmación de contraseñas antes de realizar acciones sensibles.
+ */
+
 class ConfirmPasswordController extends Controller
 {
     use ConfirmsPasswords;
+
+    /**
+     * Ruta de redirección después de confirmar la contraseña.
+     */
+    
    protected $redirectTo = '/home';
+
+     /**
+     * Constructor del controlador.
+     * 
+     * Aplica el middleware `auth` para garantizar que solo usuarios autenticados
+     * puedan acceder a las funcionalidades de confirmación de contraseña.
+     */
+
     public function __construct()
     {
         $this->middleware('auth');
